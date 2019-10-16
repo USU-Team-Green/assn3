e3ac218a62a32502ed717067be70e71cd8efd2b0
Ryan Ferguson <ryan28561@gmail.com>
2019-10-16 09:15:42 -0600

    stubbed out a start

---
+++ main.py
@@ -0,0 +1,36 @@
+
+
+BLOCK_SIZE = 16 # 16 characters or bytes
+
+def make_hash(s):
+    blocks = []
+    for index in range(0, len(s), BLOCK_SIZE):
+        block = bytes(s[index: min(index + BLOCK_SIZE, len(s))], 'utf-8')
+        block_num = 0
+        for i in range(len(block)):
+            block_num += block[i] * (256 ** i)
+        blocks.append(block_num)
+    
+    hsh = 0
+    for block in blocks:
+        temp = hsh
+        if hsh > 0:
+            hsh = (hsh * block) % (2 * 128)
+        else:
+            hsh = block % (2 ** 128)
+        hsh = hsh ^ temp
+
+    return hsh
+
+
+def test():
+    one = 'asdfjkalsdjfklasdjfklsdajfl'
+    two = 'jeiocnkonaosjdfkaldjfklejfla'
+    three = 'thebrownfoxjumpedovertheredlog'
+    four = 'thebrownfaxjumpedovertheredlog'
+
+    assert make_hash(one) == make_hash(one)
+    assert make_hash(two) == make_hash(two)
+    assert make_hash(three) == make_hash(three)
+    assert make_hash(four) == make_hash(four)
+    assert make_hash(three) != make_hash(four)
