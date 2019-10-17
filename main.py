68e313df5da1bcf85b8a803cd2763fac9201ac76
Ryan Ferguson <ryan28561@gmail.com>
2019-10-17 11:45:36 -0600

    made hash function more preimage resistant

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+from difflib import SequenceMatcher
 
 
 BLOCK_SIZE = 16 # 16 characters or bytes
@@ -10,17 +12,25 @@ def make_hash(s):
         for i in range(len(block)):
             block_num += block[i] * (256 ** i)
         blocks.append(block_num)
+
+    random.seed(0)
+    blocks_copy = blocks.copy()
+    for i in range(30):
+        temp = blocks_copy.copy()
+        random.shuffle(temp)
+        blocks = blocks + temp
     
     hsh = 0
     for block in blocks:
         temp = hsh
         if hsh > 0:
-            hsh = (hsh * block) % (2 * 128)
+            hsh = (hsh * block) % (2 ** 128)
         else:
             hsh = block % (2 ** 128)
+        hsh = (hsh << 10) & (2 ** 128 - 1)
         hsh = hsh ^ temp
 
-    return hex(hsh)
+    return hex(hsh)[2:-4]
 
 
 def test():
@@ -33,4 +43,5 @@ def test():
     assert make_hash(two) == make_hash(two)
     assert make_hash(three) == make_hash(three)
     assert make_hash(four) == make_hash(four)
-    assert make_hash(three) != make_hash(four)
+    ratio = SequenceMatcher(None, make_hash(three), make_hash(four)).ratio()
+    assert ratio < 0.7
